b66904184aca74d8fa63fb9e0f73454a29166c5b
Kashif <kashif.riaz7272@gmail.com>
2024-10-07 18:07:27 +0500

    added_files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+   
+    extend: {
+      fontFamily: {
+        customfont: ["Poppins" ,"sans-serif"],
+        // backgroundImage: {
+        //   'custom-bg': "url('./assets/bg-img.png')",
+        // },
+    },
+  },
+  plugins: [],
+}
+}
